cc5dc45dddde1e7e34cc0fe89046b987acd24c19
Cactusdude <joshmoseman@gmail.com>
2020-11-10 12:09:18 -0500

    Add files via upload

---
+++ src/Start.java
@@ -0,0 +1,10 @@
+/*
+    This class just calls the input class because of hierarchy
+ */
+
+public class Start {
+    public static void main(String []Args) {
+        Input input = new Input();
+        input.getInput();
+    }
+}
